036da9cd1f783144126bb1407194cbc7dbbee321
Victor Tabacoff <victortabacoff2007@gmail.com>
2024-08-22 23:07:53 -0500

    Initial Commit

---
+++ Shape Challenge 2/Shape_Challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Shape_Challenge_2App.swift
+//  Shape Challenge 2
+//
+//  Created by Victor Tabacoff on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Shape_Challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shape Challenge 2UITests/Shape_Challenge_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Shape_Challenge_2UITestsLaunchTests.swift
+//  Shape Challenge 2UITests
+//
+//  Created by Victor Tabacoff on 8/22/24.
+//
+
+import XCTest
+
+final class Shape_Challenge_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
